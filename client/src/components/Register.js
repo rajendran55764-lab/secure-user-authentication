cd430a065d7d7a6a654e0cf865b107f3c6834fa5
rajendran55764-lab <rajendran55764@gmail.com>
2026-06-26 21:09:23 +0530

    Update Register.js

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -16,7 +16,7 @@ function Register({ setPage }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://localhost:5000/api/auth/register', {
+      const res = await fetch('https://secure-user-authentication-yv6y.onrender.com/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
